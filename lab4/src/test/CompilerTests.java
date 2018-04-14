2eaa68a38c5498d560c1ac47bcc347a300ffd6a8
IngridSnowe <IngridSnowe@users.noreply.github.com>
2018-04-14 12:21:22 +0800

    Update CompilerTests.java
    All tests succeeded on local laptop

--- lab4/src/test/CompilerTests.java
+++ lab4/src/test/CompilerTests.java
@@ -212,5 +212,187 @@ public class CompilerTests {
 				new Object[0],
 				-42);
 	}
+	
+	//Test Expressions
+		@Test public void testNegInt() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"    return -42;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					-42);
+		}
+		
+		@Test public void testPosInt() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"    return 35;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					35);
+		}
+		
+		
+		@Test public void testString() {
+			runtest("module Test {" +
+					"  public String f() {" +
+					"    return \"good luck!\";" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					"good luck!");
+		}
+		
+		@Test public void testBoolean() {
+			runtest("module Test {" +
+					"  public boolean f() {" +
+					"    return false;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					false);
+		}
+		
+		@Test public void testArrayIndex() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"    int[] a;" + 
+					"    int b;" + 
+					"    int c;" + 
+					"	 b=2;" +
+					"	 c=5;" +
+					"    a = [2,4,6,7,-100,-20,b*c];" + 	
+					"    return a[6];}" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					10);
+		}
+		
+		@Test public void testAddExpr() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"    return 1+4;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					5);
+		}
+		
+		@Test public void testSubExpr() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"	 int a;" +
+					"	 int b;" +
+					"	 a=3;" +
+					"	 b=4;" +
+					"    return a*b;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					12);
+		}
+		
+		@Test public void testMulExpr() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"    return 100*4;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					400);
+		}
+		
+		@Test public void testDivExpr() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"    return -6/2;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					-3);
+		}
+		
+		@Test public void testModExpr() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"    return 5%2;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					1);
+		}
+		
+		@Test public void testWhileStatement() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"    int a;"+
+					"    int b;"+
+					"    a=40;"+
+					"    b=24;"+
+					"    while(a!=b){" +
+					"    	if(a>b) a=a-b;" +
+					"    	else b=b-a;" +
+					"	 }"+
+					"    return a;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					8);
+		}
+		
+		@Test public void testBreakStatement() {
+			runtest("module Test {" +
+					"  public int f() {" +
+					"    int a;"+
+					"    a=40;"+
+					"    while(true){" +
+					"    	a=a-1;" +
+					"    	if(a==4) break;"+
+					"	 }"+
+					"    return a;" +
+					"  }" +
+					"}",
+					"Test",
+					"f",
+					new Class<?>[0],
+					new Object[0],
+					4);
+		}
+	
 
 }
